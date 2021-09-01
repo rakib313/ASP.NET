b5e385df4582dbccb454731cf5f3a1b449df3858
Rakibul Islam <hridoy.9@gmail.com>
2021-09-01 18:22:01 +0600

    Changes to single photo upload

--- EmployeeManagement/Controllers/HomeController.cs
+++ EmployeeManagement/Controllers/HomeController.cs
@@ -54,32 +54,33 @@ namespace EmployeeManagement.Controllers
             {
                 string uniqueFileName = null;
 
-                // If the Photos property on the incoming model object is not null and if count > 0,
-                // then the user has selected at least one file to upload
-                if (model.Photo != null && model.Photo.Count > 0)
+                // // If the Photos property on the incoming model object is not null and if count > 0,
+                // // then the user has selected at least one file to upload
+                // if (model.Photo != null && model.Photo.Count > 0)
+                // {
+                //     // Loop thru each selected file
+                //     foreach (IFormFile photo in model.Photo)
+                //     {
+                //         // The file must be uploaded to the images folder in wwwroot
+                //         // To get the path of the wwwroot folder we are using the injected
+                //         // webHostingEnvironment service provided by ASP.NET Core
+                //         string uploadsFolder = Path.Combine(webHostEnvironment.WebRootPath, "images");
+                //         // To make sure the file name is unique, appending a new
+                //         // GUID value and and an underscore to the file name
+                //         uniqueFileName = Guid.NewGuid().ToString() + "_" + photo.FileName;
+                //         string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+                //         // Use CopyTo() method provided by IFormFile interface to
+                //         // copy the file to wwwroot/images folder
+                //         photo.CopyTo(new FileStream(filePath, FileMode.Create));
+                //     }
+                if (model.Photo != null)
                 {
-                    // Loop thru each selected file
-                    foreach (IFormFile photo in model.Photo)
-                    {
-                        // The file must be uploaded to the images folder in wwwroot
-                        // To get the path of the wwwroot folder we are using the injected
-                        // webHostingEnvironment service provided by ASP.NET Core
-                        string uploadsFolder = Path.Combine(webHostEnvironment.WebRootPath, "images");
-                        // To make sure the file name is unique, appending a new
-                        // GUID value and and an underscore to the file name
-                        uniqueFileName = Guid.NewGuid().ToString() + "_" + photo.FileName;
-                        string filePath = Path.Combine(uploadsFolder, uniqueFileName);
-                        // Use CopyTo() method provided by IFormFile interface to
-                        // copy the file to wwwroot/images folder
-                        photo.CopyTo(new FileStream(filePath, FileMode.Create));
-                    }
-
-
-                    // path to wwwroot folder
-                    // string uploadsFolder = Path.Combine(webHostEnvironment.WebRootPath, "images");
-                    // uniqueFileName = Guid.NewGuid().ToString() + "_" + model.Photo.FileName;
-                    // string filePath = Path.Combine(uploadsFolder, uniqueFileName);
-                    // model.Photo.CopyTo(new FileStream(filePath, FileMode.Create));
+                    
+                    //path to wwwroot folder
+                    string uploadsFolder = Path.Combine(webHostEnvironment.WebRootPath, "images");
+                    uniqueFileName = Guid.NewGuid().ToString() + "_" + model.Photo.FileName;
+                    string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+                    model.Photo.CopyTo(new FileStream(filePath, FileMode.Create));
                 }
                 Employee newEmployee =  new Employee() 
                 {
